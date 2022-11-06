33b639ea48974b70a8d1826af2bfb0d7b97bcdb8
rajeevrk9901 <rajeevrk9901@gmail.com>
2022-11-06 20:35:25 +0530

    news home page done

---
+++ script.js
@@ -0,0 +1,18 @@
+const deskbtn = document.getElementById("btn-nav")
+const clsbtn = document.getElementById("cls-btn")
+const active_btn = document.getElementById("desktop-navbar")
+
+
+
+console.log(deskbtn);
+
+deskbtn.addEventListener('click', () => {
+    active_btn.classList.toggle("active");
+    document.body.style.overflowY = document.body.style.overflowY === 'hidden' ? 'visible' : 'hidden';
+})
+
+clsbtn.addEventListener('click', () => {
+    active_btn.classList.toggle("active");
+    document.body.style.overflowY = document.body.style.overflowY === 'hidden' ? 'visible' : 'hidden';
+})
+
